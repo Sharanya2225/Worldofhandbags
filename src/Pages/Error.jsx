1ad24986a76927456d3cf75a7575b093291ad6e7
Sharanya2225 <sharanyaacharya035@gmail.com>
2025-03-01 11:54:01 +0000

    i- did-changes-in-alt-text

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -4,7 +4,7 @@ import NoData from "../Components/NoData";
 export default function Error() {
   return (
   <div> 404 page not found
-    <img src="https://static.vecteezy.com/system/resources/previews/001/857/111/non_2x/error-404-page-not-found-landing-page-concept-for-mobile-and-pc-free-vector.jpg" alt=""/>
+    <img src="https://static.vecteezy.com/system/resources/previews/001/857/111/non_2x/error-404-page-not-found-landing-page-concept-for-mobile-and-pc-free-vector.jpg" alt="hello git new branch 01-03-2025-s"/>
   <NoData/>
   </div>
   )}
